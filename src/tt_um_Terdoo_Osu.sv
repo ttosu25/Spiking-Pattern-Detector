4fb742b3246d3a8eb13205dcb57918cc1e1a82d1
ttosu25 <ttosu.dev@gmail.com>
2026-04-15 09:22:54 +0100

    Half instantiation

--- src/tt_um_Terdoo_Osu.sv
+++ src/tt_um_Terdoo_Osu.sv
@@ -5,7 +5,7 @@
 
 `default_nettype none
 
-module tt_um_example (
+module tt_um_Terdoo_Osu (
     input  wire [7:0] ui_in,    // Dedicated inputs
     output wire [7:0] uo_out,   // Dedicated outputs
     input  wire [7:0] uio_in,   // IOs: Input path
@@ -16,12 +16,12 @@ module tt_um_example (
     input  wire       rst_n     // reset_n - low to reset
 );
 
-  // All output pins must be assigned. If not used, assign to 0.
-  assign uo_out  = ui_in + uio_in;  // Example: ou_out is the sum of ui_in and uio_in
-  assign uio_out = 0;
-  assign uio_oe  = 0;
+    core #(.WORD_LENGTH(4),  .NEURON_COUNT(4)) pattern_detector (
+        .event_out(ui_out[0]),
+        .spike_in(ui_in[NEURON_COUNT]),
+        .clk(clk),
+        .rst(!rst_n)
 
-  // List all unused inputs to prevent warnings
-  wire _unused = &{ena, clk, rst_n, 1'b0};
+    );
 
 endmodule
