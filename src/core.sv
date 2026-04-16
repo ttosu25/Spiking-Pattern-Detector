a087d23a06dc28bb9f7f92db8654df02683f93bb
ttosu25 <ttosu.dev@gmail.com>
2026-04-16 18:34:21 +0100

    spike_out input -> output

--- src/core.sv
+++ src/core.sv
@@ -1,5 +1,5 @@
 module core #(parameter WORD_LENGTH, NEURON_COUNT) //in this design its important to remember spike_in is the MSB of a NEURON_COUNT-bit Register
-(output logic event_out, output logic indicator, input logic spike_in, input logic [NEURON_COUNT - 1: 0] spike_pattern ,input logic clk, rst);
+	(output logic event_out, output logic [NEURON_COUNT - 1: 0] spike_out ,output logic indicator, input logic spike_in, input logic [NEURON_COUNT - 1: 0] spike_pattern ,input logic clk, rst);
 
 	//control signals
 	logic n_we;
@@ -24,7 +24,7 @@ module core #(parameter WORD_LENGTH, NEURON_COUNT) //in this design its importan
 	
 ////////////////////////////////////////////////////  Registers
 	
-	logic [NEURON_COUNT - 1: 0] spike_out;
+	
 	logic signed [WORD_LENGTH - 1:0]threshold;
 	logic signed [WORD_LENGTH - 1:0] leak;
 	logic signed [WORD_LENGTH - 1:0] V_reset;
