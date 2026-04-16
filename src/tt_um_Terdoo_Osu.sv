289991f6f44b274ba7eaaa83dd1a71745d37fa38
ttosu25 <ttosu.dev@gmail.com>
2026-04-16 10:36:08 +0100

    Update tt_um_Terdoo_Osu.sv

--- src/tt_um_Terdoo_Osu.sv
+++ src/tt_um_Terdoo_Osu.sv
@@ -1,37 +1,35 @@
-/*
- * Copyright (c) 2024 Your Name
- * SPDX-License-Identifier: Apache-2.0
- */
-
 `default_nettype none
 
 module tt_um_Terdoo_Osu (
-    input  wire [7:0] ui_in,    // Dedicated inputs
-    output wire [7:0] uo_out,   // Dedicated outputs
-    input  wire [7:0] uio_in,   // IOs: Input path
-    output wire [7:0] uio_out,  // IOs: Output path
-    output wire [7:0] uio_oe,   // IOs: Enable path (active high: 0=input, 1=output)
-    input  wire       ena,      // always 1 when the design is powered, so you can ignore it
-    input  wire       clk,      // clock
-    input  wire       rst_n     // reset_n - low to reset
+    input  wire [7:0] ui_in,
+    output wire [7:0] uo_out,
+    input  wire [7:0] uio_in,
+    output wire [7:0] uio_out,
+    output wire [7:0] uio_oe,
+    input  wire       ena,
+    input  wire       clk,
+    input  wire       rst_n
 );
 
-    core #(.WORD_LENGTH(4),  .NEURON_COUNT(4)) pattern_detector (
+    localparam int WORD_LENGTH  = 4;
+    localparam int NEURON_COUNT = 4;
+
+    core #(
+        .WORD_LENGTH(WORD_LENGTH),
+        .NEURON_COUNT(NEURON_COUNT)
+    ) pattern_detector (
         .event_out(uo_out[1]),
         .indicator(uo_out[0]),
-        .spike_in(ui_in[NEURON_COUNT]), 
+        .spike_in(ui_in[4]),
         .spike_pattern(ui_in[3:0]),
         .clk(clk),
         .rst(!rst_n)
-
     );
 
-    assign ui_out[7:2] = '0;
+    assign uo_out[7:2] = '0;
 
-    //defaulting unused signals
+    // default unused bidirectional pins to input / zero
     assign uio_out = '0;
-    assign uio_in = '0;
-    assign ena = '1;
     assign uio_oe  = '0;
 
 endmodule
